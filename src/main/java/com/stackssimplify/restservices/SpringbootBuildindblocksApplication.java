e5025dd3d30f6ff14b7f65503a034a46e770c789
czarcamlo <czarcamlo@gmail.com>
2020-10-29 12:24:44 -0600

    First commit

---
+++ src/main/java/com/stackssimplify/restservices/SpringbootBuildindblocksApplication.java
@@ -0,0 +1,13 @@
+package com.stackssimplify.restservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBuildindblocksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBuildindblocksApplication.class, args);
+	}
+
+}
